caebd9865d06bca72989ced920e8c6c64c9277ed
Aditya <aditya.r@email.com>
2021-07-19 02:48:43 +0530

    1.1

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-var result = "";
-var resultColor;
+String result = "";
+var resultColor = Colors.red, labelColor = Colors.red;
 List<int> ratings = [];
 double rating = 1;
 int i = 0, total = 0;
@@ -34,68 +34,92 @@ class _HomeState extends State<Home> {
           title: Text("Feedback App"),
         ),
         body: Center(
-          child: Column(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            questionsList.elementAt(i),
+            style: TextStyle(fontSize: 24),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            width: 0,
+            height: 20,
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                questionsList.elementAt(i),
+                "${rating.toInt()}",
                 style: TextStyle(fontSize: 24),
-                textAlign: TextAlign.center,
               ),
-              SizedBox(
-                width: 0,
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "${rating.toInt()}",
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  Slider.adaptive(
-                      value: rating,
-                      min: 1,
-                      max: 5,
-                      divisions: 4,
-                      label: "${rating.toInt()}",
-                      onChanged: (newRating) {
-                        setState(() => rating = newRating);
-                      }),
-                ],
-              ),
-              SizedBox(
-                width: 0,
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    ratings.add(rating.toInt());
-                    if (i < 5) {
-                      i++;
-                      total += rating.toInt();
-                      rating = 1;
-                    } else {
-                      if (total < 10) {
-                        result = "We are sorry for your inconvenience";
-                        resultColor = Colors.red;
-                      } else if (total < 20) {
-                        result = "Hope we serve you better next time";
-                        resultColor = Colors.yellow;
-                      } else {
-                        result = "We hope you come back next time.";
-                        resultColor = Colors.green;
+              Slider.adaptive(
+                  value: rating,
+                  min: 1,
+                  max: 5,
+                  divisions: 4,
+                  activeColor: labelColor,
+                  label: "${rating.toInt()}",
+                  onChanged: (newRating) {
+                    setState(() {
+                      rating = newRating;
+                      switch (rating.toInt()) {
+                        case 1:
+                          labelColor = Colors.red;
+                          break;
+                        case 2:
+                          labelColor = Colors.orange;
+                          break;
+                        case 3:
+                          labelColor = Colors.amber;
+                          break;
+                        case 4:
+                          labelColor = Colors.lightGreen;
+                          break;
+                        case 5:
+                          labelColor = Colors.green;
+                          break;
                       }
-                      Navigator.pushNamed(context, '/ResultPage');
-                    }
-                  });
-                },
-                child: Text("Next"),
-              ),
+                    });
+                  }),
             ],
           ),
-        ));
+          SizedBox(
+            width: 0,
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                ratings.add(rating.toInt());
+                if (i < 5) {
+                  i++;
+                  labelColor = Colors.red;
+                  total += rating.toInt();
+                  rating = 1;
+                } else {
+                  if (total < 10) {
+                    result = "We are sorry for your inconvenience";
+                    resultColor = Colors.red;
+                  } else if (total < 20) {
+                    result = "Hope we serve you better next time";
+                    resultColor = Colors.amber;
+                  } else {
+                    result = "We hope you come back next time.";
+                    resultColor = Colors.green;
+                  }
+                  Navigator.pushNamed(context, '/ResultPage');
+                  setState(() {
+                    i = 0;
+                    rating = 1;
+                    labelColor = Colors.red;
+                    ratings.removeRange(0, 5);
+                  });
+                }
+              });
+            },
+            child: Text("Next"),
+          ),
+        ])));
   }
 }
 
@@ -104,7 +128,14 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Thank you"),
+        title: Text("Thank you!"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: Center(
         child: Column(
@@ -118,14 +149,18 @@ class ResultPage extends StatelessWidget {
                   fontWeight: FontWeight.bold),
             ),
             SizedBox(width: 0, height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  i = 0;
-                  ratings.removeRange(0, 5);
-                  total = 0;
-                  Navigator.pop(context);
-                },
-                child: Text("Restart"))
+            InkWell(
+              child: Text(
+                "Restart",
+                style: TextStyle(
+                    color: Colors.blue[700],
+                    fontSize: 18,
+                    decoration: TextDecoration.underline),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
           ],
         ),
       ),
